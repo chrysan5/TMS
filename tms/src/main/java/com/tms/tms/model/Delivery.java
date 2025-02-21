dbe2cb470b156ad9b623b0df30e5a7d7a228569c
chrysan5 <alswn5406@naver.com>
2025-02-21 22:09:55 +0900

    (feat) 배송 엔티티 생성

---
+++ tms/src/main/java/com/tms/tms/model/Delivery.java
@@ -0,0 +1,37 @@
+package com.tms.tms.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLRestriction;
+
+@SQLRestriction("is_delete = false")
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "p_deliveries")
+public class Delivery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "delivery_id")
+    private Long deliveryId;
+
+    private String address;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OrderLocation location; //pending, intra, inter
+
+    @Column(nullable = false)
+    private Long startHubId;
+
+    @Column(nullable = false)
+    private Long endHubId;
+
+    private boolean isDelete = false;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+}
---
+++ tms/src/main/java/com/tms/tms/repository/DeliveryRepository.java
@@ -0,0 +1,7 @@
+package com.tms.tms.repository;
+
+import com.tms.tms.model.Delivery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+}
